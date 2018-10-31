9c34e13d46d5d7f9f36a47e139023bba00a10f3e
Tong Guo <779222056@qq.com>
2018-10-31 14:22:16 +0800

    Add files via upload

--- pre_data/code/pre.py
+++ pre_data/code/pre.py
@@ -9,25 +9,28 @@ for file in dir:
     for file2 in dir_:
         dir__ = os.listdir(dir_path+"/"+file+"/"+file2)
         for file3 in dir__:
-            print(dir_path+"/"+file+"/"+file2+"/"+file3)
-            f = open(dir_path+"/"+file+"/"+file2+"/"+file3,mode="r",encoding="utf-8")
-            lines = f.readlines()
-            one_string = ""
-            for line in lines:
-                one_string+=line.replace("\n","")
-            jdata = json.loads(one_string)
-            summary = jdata["summary"].replace("\n", "")
-            if summary!="":
-                f2.write(summary)
-                f2.write("\n")
-            if len(jdata["otherInfo"])>0:
-                for text in jdata["otherInfo"]:
-                    if len(text)>0:
-                        t = text["text"].replace("\n","")
-                        sentence_list = t.split("。")
-                        for s in sentence_list:
-                            if s!="" and len(s)>=10:
-                                f2.write(s.replace("\n",""))
-                                f2.write("\n")
-            f.close()
+            try:
+                print(dir_path+"/"+file+"/"+file2+"/"+file3)
+                f = open(dir_path+"/"+file+"/"+file2+"/"+file3,mode="r",encoding="utf-8")
+                lines = f.readlines()
+                one_string = ""
+                for line in lines:
+                    one_string+=line.replace("\n","")
+                jdata = json.loads(one_string)
+                summary = jdata["summary"].replace("\n", "")
+                if summary!="":
+                    f2.write(summary)
+                    f2.write("\n")
+                if len(jdata["otherInfo"])>0:
+                    for text in jdata["otherInfo"]:
+                        if len(text)>0:
+                            t = text["text"].replace("\n","")
+                            sentence_list = t.split("。")
+                            for s in sentence_list:
+                                if s!="" and len(s)>=10:
+                                    f2.write(s.replace("\n",""))
+                                    f2.write("\n")
+                f.close()
+            except:
+                print("-")
 f2.close()
