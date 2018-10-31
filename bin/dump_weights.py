6be189c1ae25917ef9862a813b4a550775bca251
Tong Guo <779222056@qq.com>
2018-10-31 12:01:53 +0800

    Add files via upload

--- bin/dump_weights.py
+++ bin/dump_weights.py
@@ -8,7 +8,7 @@ from bilm.training import dump_weights as dw
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', default="../log/", help='Location of checkpoint files')
-    parser.add_argument('--outfile', default="../output",help='Output hdf5 file with weights')
+    parser.add_argument('--outfile', default="../dump_weights.hdf5",help='Output hdf5 file with weights')
 
     args = parser.parse_args()
     dw(args.save_dir, args.outfile)
